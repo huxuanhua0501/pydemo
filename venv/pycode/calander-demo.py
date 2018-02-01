fd1f7e6fc09cbf535b0e3bd248b80d90747a94a7
xuanhua.hu <xuanhua.hu@busonline.com>
2018-02-01 20:43:00 +0800

    2131

---
+++ venv/pycode/calander-demo.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2018/2/1 20:36
+# @Author  : huxuanhua
+# @Email   : huxhua163@163.com
+# @File    : calander-demo.py
+# @Software: PyCharm
+import calendar
+
+cal = calendar.month(2018, 1)
+print '当前月份日历:'
+print cal
